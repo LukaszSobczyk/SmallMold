14799aab24082a72ae3364e99d366c479fd770d7
LukaszSobczyk <lukasz.sobczyk@poczta.onet.pl>
2017-04-22 11:20:05 +0200

    Wstępne pliki

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	void FixedUpdate ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InputResult(Input.mousePosition);
+        }
+    }
+    void InputResult(Vector3 inputPos)
+    {
+        Vector3 touchPosWorld = Camera.main.ScreenToWorldPoint(inputPos);
+        Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+        RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+    }
+}
