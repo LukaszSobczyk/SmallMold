e4d7db4f173b7073266e199915be875f975c88c5
LukaszSobczyk <lukasz.sobczyk@poczta.onet.pl>
2017-04-25 03:14:01 +0200

    endgame

---
+++ Assets/Endgame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endgame : MonoBehaviour {
+
+    public GameObject endgame;
+	void Update () {
+        if(gameObject.GetComponent<SeedParticleSystem>()!=null)
+            if (gameObject.GetComponent<SeedParticleSystem>().IsDone())
+            {
+                endgame.SetActive(true);
+            }
+	}
+}
