1b5eb0f0ec99fb95f300ad8bb3b7ce3ed4abade0
LukaszSobczyk <lukasz.sobczyk@poczta.onet.pl>
2017-04-25 02:50:25 +0200

    no message

---
+++ Assets/Engame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engame : MonoBehaviour {
+
+	public GameObject textObject;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//if(this.GetComponent<InfectionController>().)
+	}
+}
